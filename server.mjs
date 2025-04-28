559e104558068eca0baada2e45b643f07a94cb7b
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-04-28 14:36:57 -0600

    Second Commit

--- server.mjs
+++ server.mjs
@@ -1,2 +1,26 @@
+// Imports
 import express from 'express';
 import dotenv from 'dotenv';
+
+
+
+// Setups
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3001
+
+
+// Middleware
+app.use(express.json())
+
+
+// Routes
+
+
+// Err handling middleware
+
+
+// Listener
+app.listen(PORT, () => {
+    console.log((`Server running on Port: ${PORT}`));
+});
