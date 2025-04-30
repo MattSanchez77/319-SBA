796f5244f4a4627f064b641c00f01fc1babaf07e
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-04-29 19:25:07 -0600

    Third Commit

---
+++ utilities/data.mjs
@@ -0,0 +1,54 @@
+const books = [
+    {
+      title: "The Girl with the Dragon Tattoo",
+      author: "Stieg Larsson",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "Gone Girl",
+      author: "Gillian Flynn",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "The Da Vinci Code",
+      author: "Dan Brown",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "The Reversal",
+      author: "Michael Connelly",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "Before I Go to Sleep",
+      author: "S.J. Watson",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "The Woman in the Window",
+      author: "A.J. Finn",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "Big Little Lies",
+      author: "Liane Moriarty",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "The Couple Next Door",
+      author: "Shari Lapena",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "The Girl on the Train",
+      author: "Paula Hawkins",
+      genre: "Mystery/Thriller"
+    },
+    {
+      title: "Behind Closed Doors",
+      author: "B.A. Paris",
+      genre: "Mystery/Thriller"
+    }
+  ];
+
+  export default books
