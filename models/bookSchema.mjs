0fa9c09187bcbdc18388a5a4d12a95f646ba31f2
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-05-02 10:07:20 -0600

    fourteenth Commit

--- models/bookSchema.mjs
+++ models/bookSchema.mjs
@@ -4,15 +4,20 @@ import mongoose from 'mongoose';
 const bookSchema = new mongoose.Schema({
     title: {
       type: String,
+      minLength: [5, 'Title must be at least 5 characters'],
       required: true,
+      index: true
     },
     author: {
       type: String,
       required: true,
+      index: true
     },
     genre: {
       type: String,
+      enum: ['Mystery/Thriller', 'Memoir', 'Historical Fiction'],
       required: true,
+      index: true
     }, 
   });
 
